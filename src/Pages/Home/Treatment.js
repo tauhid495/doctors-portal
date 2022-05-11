fc6b9dc08d35af0caca0647be455ebef9914303b
tauhid <tauhid495@gmail.com>
2022-05-11 21:38:14 +0600

    hi

--- src/Pages/Home/Treatment.js
+++ src/Pages/Home/Treatment.js
@@ -9,7 +9,7 @@ const Treatment = () => {
                     <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
                     <div className='md:pl-24'>
                         <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
-                        <p class="py-6">It is a long established fact that a reader will be distracted by the readable content of a page whe n looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                        <p class="py-6">It is a long established fact that a reader will be distracted by the readable content of a page whe n looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'What is your Name, Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                         <button class="btn bg-gradient-to-r from-secondary to-primary border-none text-white font-bold">Get Started</button>
                     </div>
                 </div>
